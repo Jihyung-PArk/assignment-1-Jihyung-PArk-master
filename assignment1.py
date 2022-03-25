400e368f7bfaf5c015f6d6f35513eaa210d61c9b
J.H.PARK <jihyung.park@my.jcu.edu.au>
2022-03-25 23:59:36 +1000

    version 1.1.1

--- assignment1.py
+++ assignment1.py
@@ -9,7 +9,6 @@ GitHub URL: ...
 file_name = "places.csv"
 
 
-
 def load_places(csv_file, list_of_places):
     try:
         infile = open(file_name, 'r')
@@ -38,17 +37,74 @@ def display_menu():
 def list_places(list_of_places):
     num = 0
     count_unvisit = 0
+
     for list_order in list_of_places:
         num += 1
-        print("{0}. {1} in {2} priority {3}".format(num, list_order[0], list_order[1],list_order[2]))
+        print("{0}. {1} in {2} priority {3}".format(num, list_order[0], list_order[1], list_order[2]))
         if list_order[3] == "n":
-            count_unvisit +=1
+            count_unvisit += 1
 
     print("{0} placese. You still want tp visit {1} places.".format(num, count_unvisit))
 
+# find max len of name of place and name of country
+def find_max(list_of_places):
+    count_place = 0
+    count_country = 0
+
+    for i in range(0, len(list_of_places)):
+        max_name = list_of_places[i][0]
+        if len(max_name) > count_place:
+            count_place = len(max_name)
+
+    for i in range(0, len(list_of_places)):
+        max_name = list_of_places[i][0]
+        if len(max_name) > count_country:
+            count_country = len(max_name)
+
 
 def add_new_place(list_of_places):
-    print('choice A')
+
+
+    name_input = input("Name: ")
+    while name_input == "":
+        print("Input can not be blank")
+        name_input = input("name: ")
+
+    country_input = input("Country: ")
+    while country_input == "":
+        print("Input can not be blank")
+        country_input = input("Country: ")
+
+    priority_input = input("Priority: ")
+
+    while True:
+
+        if priority_input == "":
+            print("Invalid input; enter a valid number")
+            priority_input = input("Priority: ")
+
+
+        elif int(priority_input) <= 0:
+            print("Number must be > 0")
+            priority_input = input("Priority: ")
+
+
+        elif priority_input != int:
+            print("Invalid input; enter a valid number")
+            priority_input = input("Priority: ")
+
+        else:
+            break
+
+    appending_list = []
+
+    appending_list.append(name_input)
+    appending_list.append(country_input)
+    appending_list.append(priority_input)
+    appending_list.append("n")
+
+    print("{0} in {1} (priority {2}) added to Travel Tracker".format(appending_list[0], appending_list[1], appending_list[2]))
+
 
 
 def mark_a_place_visited(list_of_places):
@@ -63,9 +119,12 @@ def main():
     print("Travel Tracker 1.0 - by Jihyung Park")
     list_of_places = []
     load_places(file_name, list_of_places)
+
     display_menu()
     choice = input("Enter a choice: ").strip().upper()
     while choice != 'Q':
+        find_max(list_of_places)
+
         if choice == 'L':
             list_places(list_of_places)
         elif choice == 'A':
