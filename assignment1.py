c3bb53ad91d26f9953916447971be14f72599b57
J.H.PARK <jihyung.park@my.jcu.edu.au>
2022-03-25 17:00:59 +1000

    version 1.0.2

--- assignment1.py
+++ assignment1.py
@@ -6,21 +6,23 @@ GitHub URL: ...
 """
 
 # Constants
-FILE_NAME = "places.csv"
+file_name = "places.csv"
 
 
 
 def load_places(csv_file, list_of_places):
     try:
-        infile = open(csv_file, 'r')
+        infile = open(file_name, 'r')
         for line in infile:
             temp_list = line.strip().split(',')
             # convert the second item, which is priority, from str to int
             temp_list[2] = int(temp_list[2])
             list_of_places.append(temp_list)
         infile.close()
-        print("{} places loaded from {}".format(len(list_of_places), FILE_NAME))
-        print(list_of_places)
+        print("{} places loaded from {}".format(len(list_of_places), csv_file))
+        # below print check list element
+        # print(list_of_places)
+
     except IOError as error:
         print("I/O error: {}".format(error))
 
@@ -34,7 +36,15 @@ def display_menu():
 
 
 def list_places(list_of_places):
-    print('choice L')
+    num = 0
+    count_unvisit = 0
+    for list_order in list_of_places:
+        num += 1
+        print("{0}. {1} in {2} priority {3}".format(num, list_order[0], list_order[1],list_order[2]))
+        if list_order[3] == "n":
+            count_unvisit +=1
+
+    print("{0} placese. You still want tp visit {1} places.".format(num, count_unvisit))
 
 
 def add_new_place(list_of_places):
@@ -50,9 +60,9 @@ def save_places(csv_file, list_of_places):
 
 
 def main():
-    print("Travel Tracker 1.0 - by student name")
+    print("Travel Tracker 1.0 - by Jihyung Park")
     list_of_places = []
-    load_places(FILE_NAME, list_of_places)
+    load_places(file_name, list_of_places)
     display_menu()
     choice = input("Enter a choice: ").strip().upper()
     while choice != 'Q':
@@ -66,7 +76,7 @@ def main():
             print("Invalid menu choice")
         display_menu()
         choice = input("Enter a choice: ").strip().upper()
-    save_places(FILE_NAME, list_of_places)
+    save_places(file_name, list_of_places)
     print("Bye.")
 
 
